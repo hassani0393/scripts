7fa08fd6dbfc79e92b2b420a7698779aec08c5b7
Mostafa Hassani <hassani0393@yahoo.com>
2021-02-05 14:09:40 +0330

    Create docker-install.sh

---
+++ docker-install.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# Created by Mostafa Hassani
+# Will install Docker in Centos
+
+# Remove old version of Docker
+sudo yum remove docker docker-client docker-client-latest docker-common docker-latest docker-latest-logrotate docker-logrotate docker-engine
+
+# Install yum-utils package to set up the repository
+sudo yum install -y yum-utils
+
+# Set up the stable repository
+sudo yum-config-mmanager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+
+# Install latest Docker Engine
+sudo yum install docker-ce docker-ce-cli containerd.io
+
+# Start Docker
+sudo systemctl start docker
+
+# Enable Docker
+sudo systemctl enable docker
+
+# Create a Docker user
+sudo useradd docker
+
+# Add the user docker to the Docker group
+sudo usermod -aG docker docker
+
+# Download Docker-Compose (update the release if needed)
+sudo curl -L "https://github.com/docker/compose/releases/download/1.28.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+# Set the permision of the binary file to executeble
+sudo chmod +x /usr/local/bin/docker-compose
